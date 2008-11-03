6db3379eea8e668d777f92372939b684da3ea90a
Steren <steren@creativecommons.org>
2008-11-03 15:07:18 +0100

    acawiki LocalSettings

--- LocalSettings.php
+++ LocalSettings.php
@@ -37,6 +37,7 @@ require_once( "$IP/includes/DefaultSettings.php" );
 # If PHP's memory limit is very low, some operations may fail.
 # ini_set( 'memory_limit', '20M' );
 
+
 if ( $wgCommandLineMode ) {
 	if ( isset( $_SERVER ) && array_key_exists( 'REQUEST_METHOD', $_SERVER ) ) {
 		die( "This script must be run from the command line\n" );
@@ -142,20 +143,20 @@ $wgCacheEpoch = max( $wgCacheEpoch, $configdate );
 # The page http://wiki.creativecommons.org/Podcasting_Legal_Guide is having a
 # problem with the default script memory limit of 16MB.  Let's bump that value
 # here:
-ini_set("memory_limit", "25M");
+ini_set("memory_limit", "48M");
 
-$wgSitename         = "CC Wiki";
+$wgSitename         = "AcaWiki";
 
 # database connectivity settings
 $wgDBserver         = "localhost";
-$wgDBname           = "cc_wiki_staging";
-$wgDBuser           = "ccwikistaging";
-$wgDBpassword       = "Ccwikistaging.";
+$wgDBname           = "acawiki";
+$wgDBuser           = "acawiki";
+$wgDBpassword       = "acawiki";
 $wgDBprefix         = "";
 $wgDBtype           = "mysql";
 
 # which skin to use
-$wgDefaultSkin = 'cc4';
+$wgDefaultSkin = 'acawiki';
 
 # cleans up the urls, so they don't include index.php everywhere
 $wgArticlePath = "/$1";
@@ -195,10 +196,33 @@ $wgLogo = "/skins/common/images/cc.logo.135.png";
 # files that would normally be served directly by Apache.  for more
 # information see: http://www.mediawiki.org/wiki/Manual:Image_Authorisation
 $wgUploadPath = "$wgScriptPath/images";
-$wgUploadDirectory = "$IP/images";
+$wgUploadDirectory = "{$IP}/images";
 
 # Extensions of allowable uploads
-$wgFileExtensions = array( 'png', 'gif', 'jpg', 'jpeg', 'ogg', 'pdf', 'svg', 'svgz', 'xcf', 'm4v', 'zip', 'odf', 'odp' );
+$wgFileExtensions = array( 'png', 'gif', 'jpg', 'jpeg', 'ogg', 'pdf', 'svg', 'svgz', 'xcf', 'm4v', 'zip', 'odf', 'odp', 'odt', 'ods', 'doc', 'xls', 'ppt' );
+
+#Search by default in...
+#Set default searching
+$wgNamespacesToBeSearchedDefault = array(
+NS_MAIN =>           true,
+NS_TALK =>           true,
+NS_USER =>           true,
+NS_USER_TALK =>      true,
+NS_PROJECT =>        false,
+NS_PROJECT_TALK =>   false,
+NS_IMAGE =>          true,
+NS_IMAGE_TALK =>     false,
+NS_MEDIAWIKI =>      false,
+NS_MEDIAWIKI_TALK => false,
+NS_TEMPLATE =>       false,
+NS_TEMPLATE_TALK =>  false,
+NS_HELP =>           false,
+NS_HELP_TALK =>      false,
+NS_CATEGORY =>       true,
+NS_CATEGORY_TALK =>  true
+);
+
+
 
 # Enable uploads
 $wgEnableUploads = true;
@@ -232,7 +256,6 @@ $wgSpamRegex = "/\<.*style.*?(display|position|overflow|visibility|height)\s*:.*
 # Nathan Kinkade 2007-12-05 14:38:07 PST
 $wgEnableParserCache = false; // don't cache parser output
 
-
 #------- http://ontoworld.org/wiki/Help:Installation -------------------
 include_once('extensions/SemanticMediaWiki/includes/SMW_Settings.php');
 enableSemantics('http://wiki.creativecommons.org');
@@ -242,15 +265,35 @@ require_once( "$IP/extensions/ParserFunctions/ParserFunctions.php" );
 require_once( "$IP/extensions/Preloader/Preloader.php" );
 	#---- SemanticForms
 include_once('extensions/SemanticForms/includes/SF_Settings.php');
+$sfgRenameEditTabs = true;
+
 	#---- Inline Query Parser Function
 # require_once('extensions/SMW_InlineQueryParserFunction.php');
 	#------ OpenID ----------------
-require_once("$IP/extensions/OpenID/OpenID.php");
+#require_once("$IP/extensions/OpenID/OpenID.php");
 
-require_once("$IP/extensions/CC_SMW_Extensions/Settings.php");
+#CC_SMW_Extensions ??? disabled because conflict with SMW1.3
+#require_once("$IP/extensions/CC_SMW_Extensions/Settings.php");
 
+
+# FIGHTING SPAM
 #------ SpamBlackList ---------
 require_once("$IP/extensions/SpamBlacklist/SpamBlacklist.php");
+
+# And SpamRegex for the ArticleComment extension which doesn't provide a SpamBlackList integration (2008/10)
+$wgSpamRegex = "/".
+"buy-viagra|dogsex|".
+"s-e-x|zoofilia|sexyongpin|grusskarte|geburtstagskarten|animalsex|".
+"sex-with|dogsex|adultchat|adultlive|camsex|sexcam|livesex|sexchat|".
+"chatsex|onlinesex|adultporn|adultvideo|adultweb.|hardcoresex|hardcoreporn|".
+"teenporn|xxxporn|lesbiansex|livegirl|livenude|livesex|livevideo|camgirl|".
+"spycam|voyeursex|casino-online|online-casino|kontaktlinsen|cheapest-phone|".
+"laser-eye|eye-laser|fuelcellmarket|lasikclinic|cragrats|parishilton|".
+"paris-hilton|paris-tape|2large|fuel-dispenser|fueling-dispenser|huojia|".
+"jinxinghj|telematicsone|telematiksone|a-mortgage|diamondabrasives|".
+"reuterbrook|sex-plugin|sex-zone|lazy-stars|eblja|liuhecai|".
+"buy-viagra|-cialis|-levitra|boy-and-girl-kissing".
+"/";
 
 #------ CategoryTree
 $wgUseAjax = true;
@@ -260,8 +303,10 @@ require_once('extensions/CategoryTree/CategoryTree.php');
 require_once( "{$IP}/extensions/Renameuser/SpecialRenameuser.php" );
 
 #------ Simple Forms
-require_once( "{$IP}/extensions/SimpleForms/SimpleForms.php" );
+#require_once( "{$IP}/extensions/SimpleForms/SimpleForms.php" );
 $wgSimpleFormsAllowRemoteAddr  = array();
+#hide the edit with form tab
+#$sfgRenameEditTabs
 
 # Add ImageMap support <grin style="evil" /> by Asheesh
 require_once('extensions/ImageMap/ImageMap.php');
@@ -291,4 +336,42 @@ $wgMainCacheType = CACHE_ACCEL;
 include_once('extensions/SemanticDrilldown/includes/SD_Settings.php');
 
 # Added this extension per request from GSoC person Hugo Dworak (nkinkade 2008-06-11)
-require_once("extensions/SyntaxHighlight_GeSHi/SyntaxHighlight_GeSHi.php");
+#require_once("extensions/SyntaxHighlight_GeSHi/SyntaxHighlight_GeSHi.php");
+
+# HeaderTabs
+require_once("$IP/extensions/HeaderTabs/HeaderTabs.php");
+
+# Contributors
+require_once( "$IP/extensions/Contributors/Contributors.php" );
+
+#Comment pages
+#(Old Extension that provides dual talk pages)
+#$wgExtraNamespaces[110] = "Comments";
+#$wgExtraNamespaces[111] = "Comments_talk";
+#$wgCommentPagesNS = 110;
+#require_once( "$IP/extensions/CommentPages/CommentPages.php" );
+
+#Article Comments (extension modified by Steren Giannini)
+require_once('extensions/ArticleComments.php');
+$wgArticleCommentsNSDisplayList = array(NS_MAIN, NS_TALK, NS_HELP, NS_HELP_TALK);
+$wgArticleCommentDefaults['displaycomments']=true;
+$wgHooks['ArticleCommentsSpamCheck'][] = 'defaultArticleCommentSpamCheck';
+
+#Widgets extension
+require_once("$IP/extensions/Widgets/Widgets.php");
+$wgGroupPermissions['sysop']['editwidgets'] = true;
+
+#Acawiki Logo
+$wgLogo = "$wgScriptPath/skins/common/images/acawiki-header.png";
+
+#HeaderFooter
+require_once( "extensions/StubManager/StubManager.php" );
+require_once( "$IP/extensions/HeaderFooter/HeaderFooter.php" );
+
+#Inputbox
+require_once($IP.'/extensions/inputbox/inputbox.php');
+
+#ImageLink
+require_once( "$IP/extensions/ImageLink/ImageLink.php" );
+
+
