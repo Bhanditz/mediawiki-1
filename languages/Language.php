Bhanditz/mediawiki-1

commit f188be7d357c5d990871c9eb4a068779ddf88e2f

Language object cache, for faster wfMsg() performance with unusual languages, and hopefully less memory. Committing for test on server.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@43398 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

languages/Language.php
@@ -74,6 +74,7 @@ class Language {
 	static public $mMergeableAliasListKeys = array( 'specialPageAliases' );
 
 	static public $mLocalisationCache = array();
+	static public $mLangObjCache = array();
 
 	static public $mWeekdayMsgs = array(
 		'sunday', 'monday', 'tuesday', 'wednesday', 'thursday',
@@ -130,11 +131,15 @@ class Language {
 	);
 
 	/**
-	 * Create a language object for a given language code
+	 * Get a language object for a given language code
 	 */
 	static function factory( $code ) {
 		global $IP;
 		static $recursionLevel = 0;
+
+		if ( isset( self::$mLangObjCache[$code] ) ) {
+			return self::$mLangObjCache[$code];
+		}
 
 		if ( $code == 'en' ) {
 			$class = 'Language';
@@ -163,6 +168,7 @@ class Language {
 			$lang = new $class;
 		}
 
+		self::$mLangObjCache[$code] = $lang;
 		return $lang;
 	}
 

commit 2591b9cc20e2002f2078562b25439814f297b420

Fixed fallback feature broken by r43398.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@43399 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

languages/Language.php
@@ -131,16 +131,21 @@ class Language {
 	);
 
 	/**
-	 * Get a language object for a given language code
+	 * Get a cached language object for a given language code
 	 */
 	static function factory( $code ) {
+		if ( !isset( self::$mLangObjCache[$code] ) ) {
+			self::$mLangObjCache[$code] = self::newFromCode( $code );
+		}
+		return self::$mLangObjCache[$code];
+	}
+
+	/**
+	 * Create a language object for a given language code
+	 */
+	protected static function newFromCode( $code ) {
 		global $IP;
 		static $recursionLevel = 0;
-
-		if ( isset( self::$mLangObjCache[$code] ) ) {
-			return self::$mLangObjCache[$code];
-		}
-
 		if ( $code == 'en' ) {
 			$class = 'Language';
 		} else {
@@ -161,14 +166,12 @@ class Language {
 		if( ! class_exists( $class ) ) {
 			$fallback = Language::getFallbackFor( $code );
 			++$recursionLevel;
-			$lang = Language::factory( $fallback );
+			$lang = Language::newFromCode( $fallback );
 			--$recursionLevel;
 			$lang->setCode( $code );
 		} else {
 			$lang = new $class;
 		}
-
-		self::$mLangObjCache[$code] = $lang;
 		return $lang;
 	}
 