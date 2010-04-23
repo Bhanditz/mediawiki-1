Bhanditz/mediawiki-1

commit a6147d501f420afdb494f8f58570878d29bfe31d

Remove optional "AS" keyword from table aliases, unbreaks installs with table prefixes
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/branches/querypage-work2/phase3@65471 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/specials/SpecialBrokenRedirects.php
@@ -27,7 +27,7 @@ class BrokenRedirectsPage extends PageQueryPage {
 
 	function getQueryInfo() {
 		return array(
-			'tables' => array( 'redirect', 'page AS p1', 'page AS p2' ),
+			'tables' => array( 'redirect', 'page p1', 'page p2' ),
 			'fields' => array( 'p1.page_namespace AS namespace',
 					'p1.page_title AS title',
 					'rd_namespace',
@@ -36,10 +36,10 @@ class BrokenRedirectsPage extends PageQueryPage {
 			'conds' => array( 'rd_namespace >= 0',
 					'p2.page_namespace IS NULL'
 			),
-			'join_conds' => array( 'page AS p1' => array( 'LEFT JOIN', array(
+			'join_conds' => array( 'page p1' => array( 'LEFT JOIN', array(
 						'rd_from=p1.page_id',
 					) ),
-					'page AS p2' => array( 'LEFT JOIN', array(
+					'page p2' => array( 'LEFT JOIN', array(
 						'rd_namespace=p2.page_namespace',
 						'rd_title=p2.page_title'
 					) )

includes/specials/SpecialDoubleRedirects.php
@@ -27,9 +27,9 @@ class DoubleRedirectsPage extends PageQueryPage {
 	function reallyGetQueryInfo( $namespace = null, $title = null ) {
 		$limitToTitle = !( $namespace === null && $title === null );
 		$retval = array (
-			'tables' => array ( 'redirect AS ra', 'redirect AS rb',
-					'page AS pa', 'page AS pb',
-					'page AS pc' ),
+			'tables' => array ( 'redirect ra', 'redirect rb',
+					'page pa', 'page pb',
+					'page pc' ),
 			'fields' => array ( 'pa.page_namespace AS namespace',
 					'pa.page_title AS title',
 					'pb.page_namespace AS nsb',

includes/specials/SpecialListredirects.php
@@ -25,7 +25,7 @@ class ListredirectsPage extends QueryPage {
 
 	function getQueryInfo() {
 		return array(
-			'tables' => array( 'page AS p1', 'redirect', 'page AS p2' ),
+			'tables' => array( 'page p1', 'redirect', 'page p2' ),
 			'fields' => array( 'p1.page_namespace AS namespace',
 					'p1.page_title AS title',
 					'rd_namespace',
@@ -34,7 +34,7 @@ class ListredirectsPage extends QueryPage {
 			'conds' => array( 'p1.page_is_redirect' => 1 ),
 			'join_conds' => array( 'redirect' => array(
 					'LEFT JOIN', 'rd_from=p1.page_id' ),
-				'page AS p2' => array( 'LEFT JOIN', array(
+				'page p2' => array( 'LEFT JOIN', array(
 					'p2.page_namespace=rd_namespace',
 					'p2.page_title=rd_title' ) ) )
 		);

commit 0835acc0995fdd0ab41eaf33edab66fe34fca1c5

* querypage-work2: Tweak DB backend so stuff like 'tables' => array( 'alias' => 'tablename' ) works * Use this to unbreak Listredirects, Brokenredirects and Doubleredirects, which were broken by r65471 (which itself attempted to fix alias-related breakage) * Fix redirect loop in Special:Mostlinkedtemplates * Remove superfluous wfSpecialMostlinkedtemplates() function
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/branches/querypage-work2/phase3@65475 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/db/Database.php
@@ -877,7 +877,7 @@ abstract class DatabaseBase {
 	/**
 	 * SELECT wrapper
 	 *
-	 * @param $table   Mixed:  Array or string, table name(s) (prefix auto-added)
+	 * @param $table   Mixed:  Array or string, table name(s) (prefix auto-added). Array keys are table aliases (optional)
 	 * @param $vars    Mixed:  Array or string, field name(s) to be retrieved
 	 * @param $conds   Mixed:  Array or string, condition(s) for WHERE
 	 * @param $fname   String: Calling function name (use __METHOD__) for logs/profiling
@@ -898,7 +898,7 @@ abstract class DatabaseBase {
 			if ( !empty($join_conds) || ( isset( $options['USE INDEX'] ) && is_array( @$options['USE INDEX'] ) ) )
 				$from = ' FROM ' . $this->tableNamesWithUseIndexOrJOIN( $table, @$options['USE INDEX'], $join_conds );
 			else
-				$from = ' FROM ' . implode( ',', array_map( array( &$this, 'tableName' ), $table ) );
+				$from = ' FROM ' . implode( ',', $this->tableNamesWithAlias( $table ) );
 		} elseif ($table!='') {
 			if ($table{0}==' ') {
 				$from = ' FROM ' . $table;
@@ -1445,6 +1445,35 @@ abstract class DatabaseBase {
 	}
 
 	/**
+	 * Get an aliased table name.
+	 * @param $name string Table name, see tableName()
+	 * @param $alias string Alias (optional)
+	 * @return string SQL name for aliased table. Will not alias a table to its own name
+	 */
+	public function tableNameWithAlias( $name, $alias ) {
+		if ( !$alias || $alias == $name ) {
+			return $this->tableName( $name );
+		} else {
+			return $this->tableName( $name ) . ' `' . $alias . '`';
+		}
+	}
+
+	/**
+	 * @param $tables array( [alias] => table )
+	 * @return array of strings, see tableNameWithAlias()
+	 */
+	public function tableNamesWithAlias( $tables ) {
+		$retval = array();
+		foreach ( $tables as $alias => $table ) {
+			if ( is_numeric( $alias ) ) {
+				$alias = $table;
+			}
+			$retval[] = $this->tableNameWithAlias( $table, $alias );
+		}
+		return $retval;
+	}
+
+	/**
 	 * @private
 	 */
 	function tableNamesWithUseIndexOrJOIN( $tables, $use_index = array(), $join_conds = array() ) {
@@ -1452,25 +1481,29 @@ abstract class DatabaseBase {
 		$retJOIN = array();
 		$use_index_safe = is_array($use_index) ? $use_index : array();
 		$join_conds_safe = is_array($join_conds) ? $join_conds : array();
-		foreach ( $tables as $table ) {
+		foreach ( $tables as $alias => $table ) {
+			if ( !is_string( $alias ) ) {
+				// No alias? Set it equal to the table name
+				$alias = $table;
+			}
 			// Is there a JOIN and INDEX clause for this table?
-			if ( isset($join_conds_safe[$table]) && isset($use_index_safe[$table]) ) {
-				$tableClause = $join_conds_safe[$table][0] . ' ' . $this->tableName( $table );
-				$tableClause .= ' ' . $this->useIndexClause( implode( ',', (array)$use_index_safe[$table] ) );
-				$tableClause .= ' ON (' . $this->makeList((array)$join_conds_safe[$table][1], LIST_AND) . ')';
+			if ( isset($join_conds_safe[$alias]) && isset($use_index_safe[$alias]) ) {
+				$tableClause = $join_conds_safe[$alias][0] . ' ' . $this->tableNameWithAlias( $table, $alias );
+				$tableClause .= ' ' . $this->useIndexClause( implode( ',', (array)$use_index_safe[$alias] ) );
+				$tableClause .= ' ON (' . $this->makeList((array)$join_conds_safe[$alias][1], LIST_AND) . ')';
 				$retJOIN[] = $tableClause;
 			// Is there an INDEX clause?
-			} else if ( isset($use_index_safe[$table]) ) {
-				$tableClause = $this->tableName( $table );
-				$tableClause .= ' ' . $this->useIndexClause( implode( ',', (array)$use_index_safe[$table] ) );
+			} else if ( isset($use_index_safe[$alias]) ) {
+				$tableClause = $this->tableNameWithAlias( $table, $alias );
+				$tableClause .= ' ' . $this->useIndexClause( implode( ',', (array)$use_index_safe[$alias] ) );
 				$ret[] = $tableClause;
 			// Is there a JOIN clause?
-			} else if ( isset($join_conds_safe[$table]) ) {
-				$tableClause = $join_conds_safe[$table][0] . ' ' . $this->tableName( $table );
-				$tableClause .= ' ON (' . $this->makeList((array)$join_conds_safe[$table][1], LIST_AND) . ')';
+			} else if ( isset($join_conds_safe[$alias]) ) {
+				$tableClause = $join_conds_safe[$alias][0] . ' ' . $this->tableNameWithAlias( $table, $alias );
+				$tableClause .= ' ON (' . $this->makeList((array)$join_conds_safe[$alias][1], LIST_AND) . ')';
 				$retJOIN[] = $tableClause;
 			} else {
-				$tableClause = $this->tableName( $table );
+				$tableClause = $this->tableNameWithAlias( $table, $alias );
 				$ret[] = $tableClause;
 			}
 		}

includes/specials/SpecialBrokenRedirects.php
@@ -26,7 +26,8 @@ class BrokenRedirectsPage extends PageQueryPage {
 
 	function getQueryInfo() {
 		return array(
-			'tables' => array( 'redirect', 'page p1', 'page p2' ),
+			'tables' => array( 'redirect', 'p1' => 'page',
+					'p2' => 'page' ),
 			'fields' => array( 'p1.page_namespace AS namespace',
 					'p1.page_title AS title',
 					'rd_namespace',
@@ -35,10 +36,10 @@ class BrokenRedirectsPage extends PageQueryPage {
 			'conds' => array( 'rd_namespace >= 0',
 					'p2.page_namespace IS NULL'
 			),
-			'join_conds' => array( 'page p1' => array( 'LEFT JOIN', array(
+			'join_conds' => array( 'p1' => array( 'LEFT JOIN', array(
 						'rd_from=p1.page_id',
 					) ),
-					'page p2' => array( 'LEFT JOIN', array(
+					'p2' => array( 'LEFT JOIN', array(
 						'rd_namespace=p2.page_namespace',
 						'rd_title=p2.page_title'
 					) )

includes/specials/SpecialDoubleRedirects.php
@@ -27,9 +27,9 @@ class DoubleRedirectsPage extends PageQueryPage {
 	function reallyGetQueryInfo( $namespace = null, $title = null ) {
 		$limitToTitle = !( $namespace === null && $title === null );
 		$retval = array (
-			'tables' => array ( 'redirect ra', 'redirect rb',
-					'page pa', 'page pb',
-					'page pc' ),
+			'tables' => array ( 'ra' => 'redirect',
+					'rb' => 'redirect', 'pa' => 'page',
+					'pb' => 'page', 'pc' => 'page' ),
 			'fields' => array ( 'pa.page_namespace AS namespace',
 					'pa.page_title AS title',
 					'pb.page_namespace AS nsb',

includes/specials/SpecialListredirects.php
@@ -25,7 +25,7 @@ class ListredirectsPage extends QueryPage {
 
 	function getQueryInfo() {
 		return array(
-			'tables' => array( 'page p1', 'redirect', 'page p2' ),
+			'tables' => array( 'p1' => 'page', 'redirect', 'p2' => 'page' ),
 			'fields' => array( 'p1.page_namespace AS namespace',
 					'p1.page_title AS title',
 					'rd_namespace',
@@ -34,7 +34,7 @@ class ListredirectsPage extends QueryPage {
 			'conds' => array( 'p1.page_is_redirect' => 1 ),
 			'join_conds' => array( 'redirect' => array(
 					'LEFT JOIN', 'rd_from=p1.page_id' ),
-				'page p2' => array( 'LEFT JOIN', array(
+				'p2' => array( 'LEFT JOIN', array(
 					'p2.page_namespace=rd_namespace',
 					'p2.page_title=rd_title' ) ) )
 		);

includes/specials/SpecialMostlinkedtemplates.php
@@ -14,7 +14,7 @@
 class MostlinkedTemplatesPage extends QueryPage {
 
 	function __construct() {
-			SpecialPage::__construct( 'MostlinkedTemplatesPage' );
+		SpecialPage::__construct( 'Mostlinkedtemplates' );
 	}
 
 	/**
@@ -113,13 +113,3 @@ class MostlinkedTemplatesPage extends QueryPage {
 	}
 }
 
-/**
- * Execution function
- *
- * @param $par Mixed: parameters passed to the page
- */
-function wfSpecialMostlinkedtemplates( $par = false ) {
-	list( $limit, $offset ) = wfCheckLimits();
-	$mlt = new SpecialMostlinkedtemplates();
-	$mlt->doQuery( $offset, $limit );
-}