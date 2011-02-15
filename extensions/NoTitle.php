be7840fb8913ca8be0848c1a3e90770c2feb473a
Nathan Kinkade <nkinkade@gandi0.creativecommons.org>
2011-02-15 19:16:35 +0000

    Added changes to NoTitle.php. Not even sure who made them.

--- extensions/NoTitle.php
+++ extensions/NoTitle.php
@@ -1,64 +1,32 @@
 <?php
  
-// ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ####
-// File:        NoTitle.php
-// Belongs-To:  MediaWiki NoTitle Extension
-// Version:     1.0-live
-// Authors:     Carlo Cabanilla / Andrew Dodd
-// Email:       andrewdodd13@gmail.com			[Expires 31/12/2009]
-// Purpose:     Hide title on pages with __NOTITLE__ magic word
-// Reqs:        None
-// ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ##### ####
+$wgExtensionCredits['parserfunction'][] = array(
+	'name' => 'No title',
+	'author' => '[http://www.mediawiki.org/wiki/User:Nx Nx]',
+	'description' => 'Adds a magic word to hide the title heading.'
+);
  
-$NoTitle = new NoTitle();
+$wgHooks['LanguageGetMagic'][] = 'NoTitle::addMagicWordLanguage';
+$wgHooks['ParserBeforeTidy'][] = 'NoTitle::checkForMagicWord';
  
-$wgHooks['MagicWordMagicWords'][] = array($NoTitle, 'addMagicWord');
-$wgHooks['MagicWordwgVariableIDs'][] = array($NoTitle, 'addMagicWordId');
-$wgHooks['LanguageGetMagic'][] = array($NoTitle, 'addMagicWordLanguage');
-$wgHooks['ParserAfterStrip'][] = array($NoTitle, 'checkForMagicWord');
-$wgHooks['BeforePageDisplay'][] = array($NoTitle, 'hideTitle');
  
 class NoTitle
 {
-    function NoTitle() {}
  
-    function addMagicWord(&$magicWords) {
-        $magicWords[] = 'MAG_NOTITLE';
-        return true;
+  static function addMagicWordLanguage(&$magicWords, $langCode) {
+    switch($langCode) {
+    default:
+      $magicWords['notitle'] = array(0, '__NOTITLE__');
     }
+    MagicWord::$mDoubleUnderscoreIDs[] = 'notitle';
+    return true;
+  }
  
-    function addMagicWordId(&$magicWords) {
-        $magicWords[] = MAG_NOTITLE;
-        return true;
+  static function checkForMagicWord(&$parser, &$text) {
+    if ( isset( $parser->mDoubleUnderscores['notitle'] ) ) {
+      $parser->mOutput->addHeadItem('<style type="text/css">/*<![CDATA[*/ .firstHeading, .subtitle, #siteSub, #contentSub, .pagetitle { display:none; } /*]]>*/</style>');
     }
+    return true;
+  }
  
-    function addMagicWordLanguage(&$magicWords, $langCode) {
-        switch($langCode) {
-        default:
-            $magicWords['MAG_NOTITLE'] = array(0, '__NOTITLE__');
-        }
-        return true;
-    }
- 
- 
-    function checkForMagicWord(&$parser, &$text, &$strip_state) {
-        global $action;
-        $mw = MagicWord::get('MAG_NOTITLE');
- 
-        if ($mw->matchAndRemove($text)) {
-            $parser->mOptions->mHideTitle = true;
-            $parser->disableCache();
-        }
- 
-        return true;
-    }
- 
-    function hideTitle(&$page) {
-        if (isset($page->parserOptions()->mHideTitle) && $page->parserOptions()->mHideTitle) {
-            $page->mScripts .= "<style type='text/css'>h1.firstHeading { display:none; } </style>";
-        }
- 
-        return true;
-    }
 }
-
