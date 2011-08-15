Bhanditz/mediawiki-1

commit 327d18dade5ce4d576164083c27ac859b3ef523a

More versions added to @deprecated tags
Couple of inbound calls fixed up

Some ancient code removed as it's been marked deprecated

git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@87584 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/filerepo/LocalFile.php
@@ -842,27 +842,6 @@ class LocalFile extends File {
 
 	/**
 	 * Record a file upload in the upload log and the image table
-	 * @deprecated use upload()
-	 */
-	function recordUpload( $oldver, $desc, $license = '', $copyStatus = '', $source = '',
-		$watch = false, $timestamp = false )
-	{
-		$pageText = SpecialUpload::getInitialPageText( $desc, $license, $copyStatus, $source );
-
-		if ( !$this->recordUpload2( $oldver, $desc, $pageText ) ) {
-			return false;
-		}
-
-		if ( $watch ) {
-			global $wgUser;
-			$wgUser->addWatch( $this->getTitle() );
-		}
-		return true;
-
-	}
-
-	/**
-	 * Record a file upload in the upload log and the image table
 	 */
 	function recordUpload2(
 		$oldver, $comment, $pageText, $props = false, $timestamp = false, $user = null

includes/Article.php
@@ -326,23 +326,6 @@ class Article {
 	}
 
 	/**
-	 * This function returns the text of a section, specified by a number ($section).
-	 * A section is text under a heading like == Heading == or \<h1\>Heading\</h1\>, or
-	 * the first section before any such heading (section 0).
-	 *
-	 * If a section contains subsections, these are also returned.
-	 *
-	 * @param $text String: text to look in
-	 * @param $section Integer: section number
-	 * @return string text of the requested section
-	 * @deprecated
-	 */
-	public function getSection( $text, $section ) {
-		global $wgParser;
-		return $wgParser->getSection( $text, $section );
-	}
-
-	/**
 	 * Get the text that needs to be saved in order to undo all revisions
 	 * between $undo and $undoafter. Revisions must belong to the same page,
 	 * must exist and must not be deleted

includes/CategoryPage.php
@@ -230,7 +230,7 @@ class CategoryViewer {
 
 	/**
 	 * Add a subcategory to the internal lists, using a title object
-	 * @deprecated kept for compatibility, please use addSubcategoryObject instead
+	 * @deprecated since 1.17 kept for compatibility, please use addSubcategoryObject instead
 	 */
 	function addSubcategory( Title $title, $sortkey, $pageLength ) {
 		$this->addSubcategoryObject( Category::newFromTitle( $title ), $sortkey, $pageLength );

includes/DefaultSettings.php
@@ -284,7 +284,7 @@ $wgAllowImageMoving = true;
 $wgIllegalFileChars = ":";
 
 /**
- * @deprecated use $wgDeletedDirectory
+ * @deprecated since 1.17 use $wgDeletedDirectory
  */
 $wgFileStore = array();
 
@@ -3599,7 +3599,7 @@ $wgSummarySpamRegex = array();
  *  - true : block it
  *  - false : let it through
  *
- * @deprecated Use hooks. See SpamBlacklist extension.
+ * @deprecated since 1.17 Use hooks. See SpamBlacklist extension.
  */
 $wgFilterCallback = false;
 
@@ -3610,7 +3610,7 @@ $wgFilterCallback = false;
 $wgEnableDnsBlacklist = false;
 
 /**
- * @deprecated Use $wgEnableDnsBlacklist instead, only kept for backward
+ * @deprecated since 1.17 Use $wgEnableDnsBlacklist instead, only kept for backward
  *  compatibility
  */
 $wgEnableSorbs = false;
@@ -3622,7 +3622,7 @@ $wgEnableSorbs = false;
 $wgDnsBlacklistUrls = array( 'http.dnsbl.sorbs.net.' );
 
 /**
- * @deprecated Use $wgDnsBlacklistUrls instead, only kept for backward
+ * @deprecated since 1.17 Use $wgDnsBlacklistUrls instead, only kept for backward
  *  compatibility
  */
 $wgSorbsUrl = array();
@@ -3672,7 +3672,7 @@ $wgRateLimitLog = null;
 /**
  * Array of groups which should never trigger the rate limiter
  *
- * @deprecated as of 1.13.0, the preferred method is using
+ * @deprecated since 1.13.0, the preferred method is using
  *  $wgGroupPermissions[]['noratelimit']. However, this will still
  *  work if desired.
  *
@@ -4426,7 +4426,7 @@ $wgLicenseTerms = false;
 
 /**
  * Set this to some HTML to override the rights icon with an arbitrary logo
- * @deprecated Use $wgFooterIcons['copyright']['copyright']
+ * @deprecated since 1.18 Use $wgFooterIcons['copyright']['copyright']
  */
 $wgCopyrightIcon = null;
 
@@ -4544,7 +4544,7 @@ $wgExtensionMessagesFiles = array();
 
 /**
  * Aliases for special pages provided by extensions.
- * @deprecated Use $specialPageAliases in a file referred to by $wgExtensionMessagesFiles
+ * @deprecated since 1.16 Use $specialPageAliases in a file referred to by $wgExtensionMessagesFiles
  */
 $wgExtensionAliasesFiles = array();
 

includes/EditPage.php
@@ -2127,7 +2127,7 @@ HTML
 	 * Produce the stock "your edit contains spam" page
 	 *
 	 * @param $match Text which triggered one or more filters
-	 * @deprecated Use method spamPageWithContent() instead
+	 * @deprecated since 1.17 Use method spamPageWithContent() instead
 	 */
 	static function spamPage( $match = false ) {
 		global $wgOut, $wgTitle;
@@ -2234,14 +2234,6 @@ HTML
 			}
 		}
 		return true;
-	}
-
-	/**
-	 * @deprecated use $wgParser->stripSectionName()
-	 */
-	function pseudoParseSectionAnchor( $text ) {
-		global $wgParser;
-		return $wgParser->stripSectionName( $text );
 	}
 
 	/**

includes/GlobalFunctions.php
@@ -2849,7 +2849,7 @@ function wfBaseConvert( $input, $sourceBase, $destBase, $pad = 1, $lowercase = t
  *
  * @param $name String
  * @param $p Array: parameters
- * @deprecated
+ * @deprecated since 1.18
  */
 function wfCreateObject( $name, $p ) {
 	return MWFunction::newObj( $name, $p );

includes/ImageGallery.php
@@ -127,9 +127,10 @@ class ImageGallery
 	 * Instruct the class to use a specific skin for rendering
 	 *
 	 * @param $skin Skin object
-	 * @deprecated Not used anymore
+	 * @deprecated since 1.18 Not used anymore
 	 */
 	function useSkin( $skin ) {
+		wfDeprecated( __METHOD__ );
 		/* no op */
 	}
 

includes/Linker.php
@@ -1990,12 +1990,6 @@ class Linker {
 		return self::makeImageLink2( $title, $file, $frameParams, $handlerParams, $time );
 	}
 
-	/** @deprecated use Linker::makeMediaLinkObj() */
-	static function makeMediaLink( $name, $unused = '', $text = '', $time = false ) {
-		$nt = Title::makeTitleSafe( NS_FILE, $name );
-		return self::makeMediaLinkObj( $nt, $text, $time );
-	}
-
 	/**
 	 * Returns the attributes for the tooltip and access key.
 	 */
@@ -2017,14 +2011,6 @@ class Linker {
 			unset( $attribs['accesskey'] );
 		}
 		return $attribs;
-	}
-
-	/**
-	 * @deprecated since 1.14
-	 * Returns raw bits of HTML, use titleAttrib() and accesskey()
-	 */
-	public static function tooltipAndAccesskey( $name ) {
-		return Xml::expandAttributes( self::tooltipAndAccesskeyAttribs( $name ) );
 	}
 
 	/**

commit 4b622ab4c879c4e927f3c37035f0b7a6a0f570bb

MFT r94543 -- fix bug 30355 (importImages regression from r87584)
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/branches/REL1_18/phase3@94585 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/filerepo/LocalFile.php
@@ -858,6 +858,25 @@ class LocalFile extends File {
 	/**
 	 * Record a file upload in the upload log and the image table
 	 */
+	function recordUpload( $oldver, $desc, $license = '', $copyStatus = '', $source = '',
+		$watch = false, $timestamp = false )
+	{
+		$pageText = SpecialUpload::getInitialPageText( $desc, $license, $copyStatus, $source );
+
+		if ( !$this->recordUpload2( $oldver, $desc, $pageText ) ) {
+			return false;
+		}
+
+		if ( $watch ) {
+			global $wgUser;
+			$wgUser->addWatch( $this->getTitle() );
+		}
+		return true;
+	}
+
+	/**
+	 * Record a file upload in the upload log and the image table
+	 */
 	function recordUpload2(
 		$oldver, $comment, $pageText, $props = false, $timestamp = false, $user = null
 	) {