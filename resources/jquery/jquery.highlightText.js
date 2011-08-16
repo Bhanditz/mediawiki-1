Bhanditz/mediawiki-1

commit d34bed81f326874dfebae58109e6bb2603511c5f

fix for bug29371 . regex wordwrap with UTF8: do not use \b metacharacter. The problem is that JavaScript recognizes word boundaries only before/after ASCII letters (and numbers/underscore)
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@90092 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

resources/jquery/jquery.highlightText.js
@@ -21,9 +21,10 @@ $.highlightText = {
 		// if this is a text node
 		if ( node.nodeType == 3 ) {
 			// TODO - need to be smarter about the character matching here. 
-			// non latin characters can make regex think a new word has begun. 
-			// look for an occurence of our pattern and store the starting position 
-			var pos = node.data.search( new RegExp( "\\b" + $.escapeRE( pat ), "i" ) );
+			// non latin characters can make regex think a new word has begun: do not use \b
+			// http://stackoverflow.com/questions/3787072/regex-wordwrap-with-utf8-characters-in-js
+			// look for an occurence of our pattern and store the starting position
+			var pos = node.data.search( new RegExp( "(^|\\s)" + $.escapeRE( pat ), "i" ) );
 			if ( pos >= 0 ) {
 				// create the span wrapper for the matched text
 				var spannode = document.createElement( 'span' );

resources/jquery/jquery.autoEllipsis.js
@@ -5,7 +5,7 @@
 
 // Cache ellipsed substrings for every string-width-position combination
 var cache = { };
-// Use a seperate cache when match highlighting is enabled
+// Use a separate cache when match highlighting is enabled
 var matchTextCache = { };
 
 $.fn.autoEllipsis = function( options ) {

resources/jquery/jquery.suggestions.js
@@ -229,7 +229,7 @@ $.suggestions = {
 				} else {
 					result = selected.prev();
 					if ( selected.length == 0 ) {
-						// we are at the begginning, so lets jump to the last item
+						// we are at the beginning, so lets jump to the last item
 						if ( context.data.$container.find( '.suggestions-special' ).html() != "" ) {
 							result = context.data.$container.find( '.suggestions-special' );
 						} else {

commit 02eccbc68b53f33286f45f802d136ff1000ce052

Fix a regression in search highlighting from r90092
Followup on r94609, r94681 test cases.
This fixes the test case with a period that was failing; problem was not the period, but the space before.
The changed regex in r90092 ended up including the preceding whitespace in the match, so the position of the match was coming up one character before the actual word -- thus offsetting the highlight by one char.

Changed from search to match, using the regex's index property (same as search returns) and adding in the length of the first match component which covers the space/whatever



git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@94702 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

resources/jquery/jquery.highlightText.js
@@ -24,8 +24,9 @@ $.highlightText = {
 			// non latin characters can make regex think a new word has begun: do not use \b
 			// http://stackoverflow.com/questions/3787072/regex-wordwrap-with-utf8-characters-in-js
 			// look for an occurence of our pattern and store the starting position
-			var pos = node.data.search( new RegExp( "(^|\\s)" + $.escapeRE( pat ), "i" ) );
-			if ( pos >= 0 ) {
+			var match = node.data.match( new RegExp( "(^|\\s)" + $.escapeRE( pat ), "i" ) );
+			if ( match ) {
+				var pos = match.index + match[1].length; // include length of any matched spaces
 				// create the span wrapper for the matched text
 				var spannode = document.createElement( 'span' );
 				spannode.className = 'highlight';