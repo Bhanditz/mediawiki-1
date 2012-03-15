Bhanditz/mediawiki-1

commit 497fbb91c5c25d6566e53c5d0c2d8c127a68a40b

* (bug 32617) Fix for edit summary on section 0 containing a bogus section heading
Based on patch posted by Dan Collins: https://bugzilla.wikimedia.org/attachment.cgi?id=9552
with a tweak to avoid breaking a similar case in other sections. Follows up on function extraction & test cases added in r105379.




git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@105380 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/EditPage.php
@@ -1720,7 +1720,7 @@ HTML
 	 * @return Mixed|string or false
 	 */
 	public static function extractSectionTitle( $text ) {
-		preg_match( "/^(=+)(.+)\\1/mi", $text, $matches );
+		preg_match( "/^(=+)(.+)\\1(\n|$)/i", $text, $matches );
 		if ( !empty( $matches[2] ) ) {
 			global $wgParser;
 			return $wgParser->stripSectionName(trim($matches[2]));

commit 3e2e420f51f120ecf31f37789e5a7d04887f854c

MFT r113922 to 1.19: Bug 35051 - [Regression] If heading has a trailing space after == then its name is not preloaded into edit summary on section edit. Regression from r105380.
git-svn-id: http://svn.wikimedia.org/svnroot/mediawiki/branches/REL1_19/phase3@113923 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

RELEASE-NOTES-1.19
@@ -20,6 +20,8 @@ production.
   "movepage-moved"
 * (bug 34841) Edit links are no longer displayed when display old page versions
 * (bug 34889) User name should be normalized on Special:Contributions
+* (bug 35051) If heading has a trailing space after == then its name is not 
+  preloaded into edit summary on section edit
 
 === Configuration changes in 1.19 ===
 * Removed SkinTemplateSetupPageCss hook; use BeforePageDisplay instead.

includes/EditPage.php
@@ -1867,7 +1867,7 @@ class EditPage {
 	 * @return Mixed|string or false
 	 */
 	public static function extractSectionTitle( $text ) {
-		preg_match( "/^(=+)(.+)\\1(\n|$)/i", $text, $matches );
+		preg_match( "/^(=+)(.+)\\1\\s*(\n|$)/i", $text, $matches );
 		if ( !empty( $matches[2] ) ) {
 			global $wgParser;
 			return $wgParser->stripSectionName(trim($matches[2]));